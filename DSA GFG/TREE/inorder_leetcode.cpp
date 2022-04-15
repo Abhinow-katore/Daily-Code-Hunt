d0b5a10a83761a4a2671d815f2b1791a211b89ec
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-04-15 13:50:25 +0530

    Update inorder_leetcode.cpp

--- DSA GFG/TREE/inorder_leetcode.cpp
+++ DSA GFG/TREE/inorder_leetcode.cpp
@@ -31,3 +31,62 @@ public:
         return node;
     }
 };
+//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isSymmetricfast(Node *rootleft, Node *rootright)
+    {
+        if (rootleft == NULL && rootright == NULL)
+        {
+            return true;
+        }
+        if (rootleft != NULL && rootright == NULL)
+        {
+            return false;
+        }
+        if (rootleft == NULL && rootright != NULL)
+        {
+            return false;
+        }
+
+        if (rootleft->data != rootright->data)
+        {
+            return false;
+        }
+
+        bool cnd1 = isSymmetricfast(rootleft->left, rootright->right);
+        bool cnd2 = isSymmetricfast(rootleft->right, rootright->left);
+
+        if (cnd1 == true && cnd2 == true)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool isSymmetric(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
+        return isSymmetricfast(root->left, root->right);
+    }
+}
+}
+;
