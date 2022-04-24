a293cdd0e4e1e5198f93cae7e1f23a2f927c031e
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-04-24 20:16:02 +0530

    23 April Codeforce

---
+++ CODEFORCE/23 April/c.cpp
@@ -0,0 +1,57 @@
+// #include <bits/stdc++.h>
+// #include <stdio.h>
+// using namespace std;
+
+// int main()
+// {
+//     int test;
+//     for (int o = 0; o < test; o++)
+//     {
+//         int n = s.size();
+//         if (s[n - 1] 'A')
+//         {
+//             ==
+
+//                 cout << "NO" << endl;
+
+//             continue;
+
+//             {
+//                 f(n == 1)
+//                 {
+//                     ₁ cout << "NO" << endl;
+//                     continue;
+//                 }
+
+//                 vector < int a(n), b(n);
+//                 int_counter = 0;
+//                 bool flag = false;
+//                 for (int i = 0; i < n; i++)
+//                 {
+//                     if (s[i] == 'A')
+//                         counter++;
+//                     a[i] = counter;
+//                 }
+
+//                 counter = 0;
+//                 for (int i = 0; i < n; i++)
+//                 {
+//                     if (s[i] == 'B')
+//                         counter++;
+//                     b[i] = counter;
+//                 }
+//                 for (int i = 0; i < n; i++)
+//                 {
+//                     if (s[i] == 'A')
+//                         continue;
+//                     else
+//                     {
+//                         if (b[i] > a[i])
+//                             flag = true;
+//                     }
+//                 }
+//                 cout << (flag ? "NO" : "YES") << endl;
+//             }
+
+//             return 0;
+//         }
