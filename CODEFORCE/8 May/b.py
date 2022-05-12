e5c80d87edc91d4a32ec7a708b6e05cc738d6758
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-05-12 19:24:33 +0530

    b

---
+++ CODEFORCE/8 May/b.py
@@ -0,0 +1,43 @@
+for in range(int (input())):
+
+size = int(input())
+
+1 = input()
+
+1 = [x for x in l]
+
+count = 0
+
+oddsum = 0
+
+evensum = 0
+
+if(l[0]=='0'): evensum+ 1
+
+else:
+
+oddsum+= 1
+
+for i in range(1, size): if(l[i]=='0'):
+
+if(l[i-1]=='1' and oddsum%2==1): count+= 1
+
+U[i]==*1*
+
+oddsum= 0
+
+else:
+
+evensum+= 1
+
+else: if(l[i-1]=='0' and evensum%2==1):
+
+count+= 1 U[i]='9'
+
+evensum= 0
+
+else:
+
+oddsum+= 1
+
+print(count)
