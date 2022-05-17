290f7b793983463918ad1a4e801b83d323bd79d1
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-05-17 14:31:08 +0530

    nque c++

--- nqueen.cpp
+++ nqueen.cpp
@@ -3,41 +3,49 @@
 using namespace std;
 
 
-
-
-bool solveNQUtil(int board[N][N], int col){
-    if(col>=N){
-        return true;
-    }
+bool safe(int board[N][N], int row, int col){
+    int i, j;
+    for ( i = 0; i < col; i++)
+        if(board[row][i])
+            return false;
+    for ( i =row, j =col; i>=0 && j>=0 ; i--,j--)
+        if(board[i][j])
+            return false;
+    for ( i =row, j =col; j>=0 && i<N ; i++, j--) 
+        if(board[i][j]) 
+            return false;
+    return true;
+}
+bool solveQut(int board[N][N] ,int col){
+    if(col>=N)return true;
     for (int i = 0; i < N; i++)
     {
-        if(isSafe(board,i,col)){
+        if(safe(board, i, col)){
             board[i][col]=1;
-
-            if(isSafe(board,i,col+1)) return true;
-            board[i][col] =0;
+            if(solveQut(board, col+1)){
+                return true;}
+            board[i][col]=0; //backtracking
         }
-
     }
+    return false;
     
 }
 
-bool solveNQ(){
+int main(){
     int board[N][N] = { { 0, 0, 0, 0 },
                         { 0, 0, 0, 0 },
                         { 0, 0, 0, 0 },
                         { 0, 0, 0, 0 } };
-
-    if(solveNQUtil(board,0)==false){
-        cout<<"not exist";
-        return false;
+ 
+    if(solveQut(board,0)==false){
+        cout<<"can't"<<"\n";
+        return 0;
     }
-    return true;
-}
-
-
-int main()
-{
-    solveNQ();
-    return 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            cout << " " << board[i][j] << " ";
+        printf("\n");
+    }
+    
+    
 }
