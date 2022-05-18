60df4d99297706f215b9072ed7c51f3db7781f38
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-05-18 09:11:48 +0530

    practice codes

---
+++ chatbot3.py
@@ -0,0 +1,19 @@
+import mailbox
+from tkinter import*
+root = Tk()
+root.title("Chatbot")
+
+def send():
+    txt.insert(END, " You->" + e.get())
+    user = e.get().lower()
+    if(user=="hi"):
+        txt.insert(END, " "+ "\n Bot->heloo\n")
+    e.delete(0,END)
+
+txt = Text(root)
+txt.grid()
+e = Entry(root)
+e.grid()
+send = Button(root, text = "send", command = send)
+send.grid()
+root.mainloop()
