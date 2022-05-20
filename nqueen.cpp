5e6c089fcda620d39fc15b642ffc041454e41994
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-05-20 18:20:33 +0530

    indent 1

--- nqueen.cpp
+++ nqueen.cpp
@@ -2,49 +2,56 @@
 #define N 4
 using namespace std;
 
-bool safe(int board[N][N], int row, int col){
+bool safe(int board[N][N], int row, int col)
+{
     int i, j;
-    for ( i = 0; i < col; i++)
-        if(board[row][i])
+    for (i = 0; i < col; i++)
+        if (board[row][i])
             return false;
-    for ( i =row, j =col; i>=0 && j>=0 ; i--,j--)
-        if(board[i][j])
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+        if (board[i][j])
             return false;
-    for ( i =row, j =col; j>=0 && i<N ; i++, j--) 
-        if(board[i][j]) 
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+        if (board[i][j])
             return false;
     return true;
 }
-bool solveQut(int board[N][N] ,int col){
-    if(col>=N)return true;
+bool solveQut(int board[N][N], int col)
+{
+    if (col >= N)
+        return true;
     for (int i = 0; i < N; i++)
     {
-        if(safe(board, i, col)){
-            board[i][col]=1;
-            if(solveQut(board, col+1)){
-                return true;}
-            board[i][col]=0; //backtracking
+        if (safe(board, i, col))
+        {
+            board[i][col] = 1;
+            if (solveQut(board, col + 1))
+            {
+                return true;
+            }
+            board[i][col] = 0; // backtracking
         }
     }
     return false;
-    
 }
 
-int main(){
-    int board[N][N] = { { 0, 0, 0, 0 },
-                        { 0, 0, 0, 0 },
-                        { 0, 0, 0, 0 },
-                        { 0, 0, 0, 0 } };
- 
-    if(solveQut(board,0)==false){
-        cout<<"can't"<<"\n";
+int main()
+{
+    int board[N][N] = {{0, 0, 0, 0},
+                       {0, 0, 0, 0},
+                       {0, 0, 0, 0},
+                       {0, 0, 0, 0}};
+
+    if (solveQut(board, 0) == false)
+    {
+        cout << "can't"
+             << "\n";
         return 0;
     }
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         for (int j = 0; j < N; j++)
             cout << " " << board[i][j] << " ";
         printf("\n");
     }
-    
-    
 }
