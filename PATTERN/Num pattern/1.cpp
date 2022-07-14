135830218552665a813c32708961fbcc7b2ff0c7
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-14 22:42:41 +0530

    change

--- PATTERN/Num pattern/1.cpp
+++ PATTERN/Num pattern/1.cpp
@@ -2,24 +2,36 @@
 #include <stdio.h>
 using namespace std;
 
-int main(){
-    int n =6;
-    int val= 0;
-    for (int i = 1; i <n+1; i++)
+int main()
+{
+    int n = 6;
+    int val = 0;
+
+    for (int i = 1; i < n + 1; i++)
     {
-        for (int j = 0; j <i ; j++)
+        for (int j = 0; j < i; j++)
         {
-            cout<<val+1;
-            val = val+1;
+            cout << val + 1;
+            val = val + 1;
             // if(j!=0||j!=n-1)cout<<"*";
-            if(j==i-1)continue;
-            else cout<<"*";
+            if (j == i - 1)
+                continue;
+            else
+                cout << "*";
         }
-        cout<<"\n";
-        
-        
+        cout << "\n";
     }
-    
-    
+    for (int i = 0; i < n+1; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            cout << val - n + j + 1;
+            // if(j==n)continue;
+                cout << "*";
+        }
+        val = val - n + i;
+        cout << "\n";
+    }
+
     return 0;
 }
---
+++ PATTERN/Num pattern/sort.cpp
@@ -0,0 +1,16 @@
+#include <bits\stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int k = 3;
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int j = 0; j < k; j++)
+    {
+        if (arr[j])
+    }
+
+    return 0;
+}
