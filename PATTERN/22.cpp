b96a1e4de0cb69e31836beb7f4c2adfbac78a2bc
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-14 16:33:04 +0530

    Update 22.cpp

---
+++ PATTERN/22.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h>
+// #include <stdio.h>
+// using namespace std;
+
+// int main(){
+//     int x =0 ;
+//     for (int i = 0; i < 7; i++)
+//     {
+        
+//         for (int j = 0; j < 7; j++)
+//         {
+            
+//             if(i)
+//         }
+//         x++;
+        
+//     }
+    
+//     return 0;
+// }
