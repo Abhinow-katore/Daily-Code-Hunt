1b716f293974ed1d86c8c322631a67898bf5c479
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-14 22:47:11 +0530

    change2

--- PATTERN/Num pattern/sort.cpp
+++ PATTERN/Num pattern/sort.cpp
@@ -5,11 +5,38 @@ using namespace std;
 int main()
 {
     int k = 3;
-    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int arr[8] = {2,5,6,4,1,3,9,8};
     int n = sizeof(arr) / sizeof(arr[0]);
-    for (int j = 0; j < k; j++)
+    for (int l = 0; l < k; l++)
     {
-        if (arr[j])
+        for (int j = 0; j < k - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int a = arr[j];
+                int b = arr[j + 1];
+                arr[j] = b;
+                arr[j + 1] = a;
+            }
+        }
+    }
+    for (int i = k; i < n; i++)
+    {
+        for (int j = k; j < n - 1; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                int a = arr[j];
+                int b = arr[j + 1];
+                arr[j] = b;
+                arr[j + 1] = a;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
     }
 
     return 0;
