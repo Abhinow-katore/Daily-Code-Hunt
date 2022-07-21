f50c4898da84115bebceb866fffd4ae3cafcb245
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-21 22:14:27 +0530

    list practice

---
+++ SORT/tes.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    int i = 5, j = 2;
+    printf("%d %d ", i<<j, i>>j);
+    // cout<<2<<5;
+    cout<<i<<j;
+    return 0;
+}
---
+++ list_linked_practrice.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    // Node* head = NULL;
+    // Node* temp = NULL;
+    // temp = node->next;
+    // temp = temp->next;
+    // temp->next = head;
+    // temp->val;
+    // temp->next->val;
+    
+    return 0;
+}
