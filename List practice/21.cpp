2fd0e66a920f1e64f12c5f34a0ea75a9a6ce1206
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-21 22:23:14 +0530

    Update 21.cpp

--- List practice/21.cpp
+++ List practice/21.cpp
@@ -1,23 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class ListNode
-{
-    public:
-    int data;
-    ListNode* next;
-};
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class ListNode
+// {
+//     public:
+//     int data;
+//     ListNode* next;
+// };
  
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         ListNode* h1 = list1;
+//         ListNode* h2 = list2;
+//         while (h1->next!= NULL && h2->next!=NULL)
+//         {
+//             /* code */
+//         }
         
-    }
-};
+//     } 
+// };
