1c158c972710a628d9a6c6996b428d39b8f41795
Abhinow-katore <67780238+Abhinow-katore@users.noreply.github.com>
2022-07-23 10:23:01 +0530

    Update list_linked_practrice.cpp

--- list_linked_practrice.cpp
+++ list_linked_practrice.cpp
@@ -1,15 +1,15 @@
-#include <bits/stdc++.h>
-#include <stdio.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// #include <stdio.h>
+// using namespace std;
 
-int main(){
-    // Node* head = NULL;
-    // Node* temp = NULL;
-    // temp = node->next;
-    // temp = temp->next;
-    // temp->next = head;
-    // temp->val;
-    // temp->next->val;
-    
-    return 0;
-}
+// int main(){
+//     // Node* head = NULL;
+//     // Node* temp = NULL;
+//     // temp = node->next;
+//     // temp = temp->next;
+//     // temp->next = head;
+//     // temp->val;
+//     // temp->next->val;
+
+//     return 0;
+// }
